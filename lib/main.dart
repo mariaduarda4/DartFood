b8b05902cba9ad50bab28eb6090584e8b34a611a
FlutLab.io <>
2022-07-17 06:50:00 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -185,6 +185,155 @@ class DartFood extends StatelessWidget {
                 ),
               ),
             ),
+            Container(
+              height: 10,
+              color: Colors.grey.shade100,
+            ),
+            Container(
+              height: 150,
+              padding: EdgeInsets.only(top: 12, left: 12),
+              alignment: Alignment.topLeft,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    'Restaurantes Famosos',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 17),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Expanded(
+                    child: ListView(
+                      scrollDirection: Axis.horizontal,
+                      children: <Widget>[
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(4),
+                              child: Image(
+                                image: NetworkImage('https://th.bing.com/th/id/R.5855c10a0a1b959b1bd7600c0089ff08?rik=bEoA9nX4i7cVzA&pid=ImgRaw&r=0'),
+                                height: 70,
+                              ),
+                            ),
+                            SizedBox(
+                              height: 7,
+                            ),
+                            Text(
+                              'Bobs - Arapiraca',
+                              style: TextStyle(fontWeight: FontWeight.w500, color: Colors.black54),
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(4),
+                              child: Image(
+                                image: NetworkImage('https://logos-download.com/wp-content/uploads/2016/03/McDonalds_Logo_2006.png'),
+                                height: 70,
+                              ),
+                            ),
+                            SizedBox(
+                              height: 7,
+                            ),
+                            Text(
+                              'McDonalds - Arapiraca',
+                              style: TextStyle(fontWeight: FontWeight.w500, color: Colors.black54),
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(4),
+                              child: Image(
+                                image: NetworkImage('https://th.bing.com/th/id/R.e1b573afc28a37d1a06e333350b46fd7?rik=XnoXo29PIpakCA&pid=ImgRaw&r=0'),
+                                height: 70,
+                              ),
+                            ),
+                            SizedBox(
+                              height: 7,
+                            ),
+                            Text(
+                              'Burger King - Arapiraca',
+                              style: TextStyle(fontWeight: FontWeight.w500, color: Colors.black54),
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(4),
+                              child: Image(
+                                image: NetworkImage('https://img.freepik.com/premium-vector/american-pizza-logo-with-master-chef-icon-fast-food-restaurant-logo_513640-341.jpg?w=2000'),
+                                height: 70,
+                              ),
+                            ),
+                            SizedBox(
+                              height: 7,
+                            ),
+                            Text(
+                              'American pizza - Arapiraca',
+                              style: TextStyle(fontWeight: FontWeight.w500, color: Colors.black54),
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(4),
+                              child: Image(
+                                image: NetworkImage('https://media-cdn.tripadvisor.com/media/photo-s/13/ca/c1/d3/nossa-logomarca.jpg'),
+                                height: 70,
+                              ),
+                            ),
+                            SizedBox(
+                              height: 7,
+                            ),
+                            Text(
+                              'Sushi mania - Arapiraca',
+                              style: TextStyle(fontWeight: FontWeight.w500, color: Colors.black54),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              height: 10,
+              color: Colors.grey.shade100,
+            ),
+            Container(
+              padding: EdgeInsets.all(12),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(4),
+                child: Image(
+                  image: NetworkImage('https://thumbs.dreamstime.com/b/faixa-de-desconto-italiano-sobre-comida-em-fundo-escuro-conceito-menu-pizza-com-pepperoni-margherita-fatias-realistas-cortantes-186380456.jpg'),
+                  height: 70,
+                ),
+              ),
+            ),
           ], //widget
         ),
       ),
