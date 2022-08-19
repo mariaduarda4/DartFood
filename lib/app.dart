4b1121690540b7419a927ac66e53f0d051f2b98d
FlutLab.io <>
2022-08-19 22:46:50 +0000

    Autosave

--- lib/app.dart
+++ lib/app.dart
@@ -31,7 +31,7 @@ class _DartFoodState extends State<DartFood> {
         body: ListView(
           children: <Widget>[
             Container(
-              height: 20,
+              height: 120,
               color: Colors.yellow,
               padding: EdgeInsets.only(top: 12, left: 12),
               alignment: Alignment.topLeft,
