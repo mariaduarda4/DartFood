84499f8ccd30747ed92122ac04031553b8f0f227
FlutLab.io <>
2022-08-20 03:15:48 +0000

    Autosave

--- lib/app.dart
+++ lib/app.dart
@@ -46,7 +46,7 @@ class _DartFoodState extends State<DartFood> {
                 children: <Widget>[
                   Text(
                     'Restaurantes  Bebidas  Pizzas  Saladas  Sobremesas',
-                    style: TextStyle(fontSize: 16),
+                    style: TextStyle(fontSize: 15),
                   ),
                   SizedBox(
                     height: 12,
